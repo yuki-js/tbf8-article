53124427baa808a2c6d8212059bd3f1db5331acb
yuki-js <20838151+yuki-js@users.noreply.github.com>
2020-02-03 23:10:45 +0900

    split hashing

--- runtime/src/certstore.rs
+++ runtime/src/certstore.rs
@@ -2,24 +2,23 @@ use frame_support::{
     decl_event, decl_module, decl_storage,
     dispatch::{Decode, DispatchResult, Encode, Vec},
 };
-use sp_core::{Blake2Hasher, Hasher, hash::H256};
+use sp_core::{hash::H256, Blake2Hasher, Hasher};
 use system::ensure_signed;
 pub trait Trait: system::Trait {
     type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
 
 type CertificateData = Vec<u8>;
-type Signature = [u8; 32];
+type Sig = H256; // Signatureって名前を使うとバグるっぽい
 
 #[derive(Decode, Encode, Clone, PartialEq, Default)]
 #[cfg_attr(feature = "std", derive(Debug))]
 pub struct Certificate {
     pub data: CertificateData,
     pub hash: H256,
-    pub sigs: Vec<Signature>
+    pub sigs: Vec<Sig>,
 }
 
-/// ストレージのデータ構造を作る
 decl_storage! {
     trait Store for Module<T: Trait> as CertStore {
         /// 証明書のハッシュテーブル
@@ -38,7 +37,7 @@ decl_event!(
     where
         AccountId = <T as system::Trait>::AccountId,
     {
-        /// 証明書が追加された時のイベント
+        /// 証明書が追加されました!
         CertAdded(AccountId, H256),
     }
 );
@@ -48,10 +47,17 @@ decl_module! {
         fn deposit_event() = default;
 
         /// 証明書追加
-        pub fn add_cert(origin, data: CertificateData, sigs: Vec<Signature>) -> DispatchResult {
+        pub fn add_cert(origin, data: CertificateData, sigs: Vec<Sig>) -> DispatchResult {
             let sender = ensure_signed(origin)?;
             // some check
-            let hash = Self::insert_cert(data, sigs)?;
+
+            let hash = Blake2Hasher::hash(&data[..]);
+            let cert = Certificate {
+                data: data,
+                sigs: sigs,
+                hash: hash.clone(),
+            };
+            Self::insert_cert(hash, cert)?;
             Self::deposit_event(RawEvent::CertAdded(sender, hash));
             Ok(())
         }
@@ -60,23 +66,12 @@ decl_module! {
 
 impl<T: Trait> Module<T> {
     /// 証明書を記録する
-    pub fn insert_cert(
-        data: CertificateData,
-        sigs: Vec<Signature>,
-    ) -> Result<H256, &'static str> {
-        let hash = Blake2Hasher::hash(&data[..]);
-        Certificates::insert(
-            &hash,
-            Certificate {
-                data: data,
-                sigs: sigs,
-                hash: hash.clone(),
-            },
-        );
+    pub fn insert_cert(hash: H256, cert: Certificate) -> DispatchResult {
+        Certificates::insert(&hash, cert);
         let current_index = CertificateCount::get();
         CertificateArray::insert(current_index, &hash);
         let next_index = current_index.checked_add(1).ok_or("index overflowed")?;
         CertificateCount::put(next_index);
-        Ok(hash)
+        Ok(())
     }
 }
